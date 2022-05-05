13da40a63d67dbd53b7b5893630998b9cb9df9a8
BenjeeDaBoi <bha9465@students.fhv.at>
2022-05-05 21:27:43 +0200

    Date time output on error + updated questions

--- webcam-viewer/webcam-viewer.py
+++ webcam-viewer/webcam-viewer.py
@@ -1,5 +1,6 @@
 from threading import Thread
 from dotenv import load_dotenv
+from datetime import datetime
 from time import sleep
 
 import cv2
@@ -48,7 +49,7 @@ class WebcamViewer:
             
             # If program was closed wrongly
             if closeProgram == False:
-                print("[ERROR] Not Connection to RTSP Stream, reconnecting in 3 seconds. . .")
+                print("[ERROR] [", datetime.now().strftime('%H:%M:%S'), "] Not Connection to RTSP Stream, reconnecting in 3 seconds. . .")
                 sleep(3)
             else:
                 print("[DEBUG] Ending RTSP Stream . . .")
